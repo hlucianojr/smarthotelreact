2d82b20d788bc57249f9e5fc40a93846233d0a50
Hector Luciano Jr <hlucianojr@gmail.com>
2021-05-10 13:35:32 -0400

    p

--- SmartMobileBare/App.js
+++ SmartMobileBare/App.js
@@ -1,14 +1,90 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+import { Asset } from 'expo-asset';
+import { Block, GalioProvider } from 'galio-framework';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hector is the man,</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Screens from './app/navigation/Screens';
+import { Images, articles, nowTheme } from './app/constants';
+
+// cache app images
+const assetImages = [
+  Images.Onboarding,
+  Images.Logo,
+  Images.Pro,
+  Images.NowLogo,
+  Images.iOSLogo,
+  Images.androidLogo,
+  Images.ProfilePicture,
+  Images.CreativeTimLogo,
+  Images.InvisionLogo,
+  Images.RegisterBackground,
+  Images.ProfileBackground
+];
+
+// cache product images
+articles.map(article => assetImages.push(article.image));
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+}
+
+export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false,
+    fontLoaded: false
+  };
+  render() {
+    if (!this.state.isLoadingComplete) {
+      return (
+        <AppLoading
+          startAsync={this._loadResourcesAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    } else {
+    return ( 
+      <NavigationContainer>
+          <GalioProvider theme={nowTheme}>
+            <Block flex>
+              <Screens />
+            </Block>
+          </GalioProvider>
+        </NavigationContainer>
+    );
+    }
+}
+  _loadResourcesAsync = async () => {
+    await Font.loadAsync({
+      'montserrat-regular': require('./app/assets/font/Montserrat-Regular.ttf'),
+      'montserrat-bold': require('./app/assets/font/Montserrat-Bold.ttf')
+    });
+
+    this.setState({ fontLoaded: true });
+    return Promise.all([...cacheImages(assetImages)]);
+  };
+
+  _handleLoadingError = error => {
+    // In this case, you might want to report the error to your error
+    // reporting service, for example Sentry
+    console.warn(error);
+  };
+
+  _handleFinishLoading = () => {
+    if (this.state.fontLoaded) {
+      this.setState({ isLoadingComplete: true });
+    }
+};
 }
 
 const styles = StyleSheet.create({
